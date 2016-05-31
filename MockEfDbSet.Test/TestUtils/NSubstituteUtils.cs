2439198c9717b005d1b4fbfe1d47b7f0f7d7cc0d
AlexL70 <alexander.levinson.70@gmail.com>
2016-05-31 19:01:50 +0300

    Methods added for creating mock entity out of POCO object and setting virtual properties

--- MockEfDbSet.Test/TestUtils/NSubstituteUtils.cs
+++ MockEfDbSet.Test/TestUtils/NSubstituteUtils.cs
@@ -3,6 +3,7 @@ using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using NSubstitute;
+using System;
 
 namespace MockEfDbSet.Test.TestUtils
 {
@@ -29,6 +30,28 @@ namespace MockEfDbSet.Test.TestUtils
             }
 
             return mockSet;
+        }
+
+        public static T CreateMockEntity<T>(T entity)
+        {
+            var props = typeof(T).GetProperties().Where(p => !p.GetMethod.IsVirtual);
+            var values = new List<object>();
+            foreach (var prop in props)
+            {
+                values.Add(prop.GetValue(entity));
+            }
+            var mEntity = Substitute.For(new Type[] { typeof(T)}, values.ToArray());
+            return (T)mEntity;
+        }
+
+        public static void SetMockVirtualProp<T, TResult>(T entity, string propName, Func<T, TResult> expr)
+        {
+            var prop = typeof(T).GetProperty(propName);
+            if (!prop.GetMethod.IsVirtual)
+            {
+                throw new ArgumentException($"{propName} is not a virtual property.");
+            }
+            prop.GetValue(entity).Returns(expr(entity));
         }
     }
 }
